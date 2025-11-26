ccaacbfb418311bf641a04f4c9fce6e9ba283d67
salmanavicenna0-max <salmanavicenna0@gmail.com>
2025-11-26 09:34:42 +0700

    projek Pa culay

---
+++ mading2/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GwvHdCa1_ASPVOjyGIzNt6Fv4dSSLiYZSw&s" alt="Avatar">
+<div class="container">
+  <h2>Login</h2>
+  <p>Silahkan Login Untuk Masuk ke Halaman Utama</p>
+  <form>
+    <div class="form-group">
+      <label for="usr"> Email:</label>
+      <input type="text" class="form-control" id="usr">
+    </div>
+    <div class="form-group">
+      <label for="pwd"> Password:</label>
+      <input type="password" class="form-control" id="pwd">
+    </div>
+    <a href="register.php">Belum Punya akun</a>
+  
+  <button type="button" class="btn btn-primary btn-block">Login</button>
+  </form>
+</div>
+
+
+
+
+</body>
+</html>
